1d09c654ffd79b4cf862aaf452222edbe7e0b31f
pavlos <pavnicolaou@gmail.com>
2019-04-21 15:08:06 +0100

    bypassed storyboards and created UITableViewController

--- GroupedMessages/ViewController.swift
+++ GroupedMessages/ViewController.swift
@@ -8,13 +8,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    fileprivate let cellId = "id123"
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        navigationItem.title = "Messages"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
+        
+        
     }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+        
+        cell.textLabel?.text = "We want to provide a longer string that is actually going to wrap into the next line and maybe even a third line."
 
+        cell.textLabel?.numberOfLines = 0
+        return cell
+    }
 
 }
 
