7def08b568873bc1fb8c4c2d50a80032dbde9e49
pavlos <pavnicolaou@gmail.com>
2019-04-21 15:31:20 +0100

    created the bubble around the text

--- GroupedMessages/ChatMessageCell.swift
+++ GroupedMessages/ChatMessageCell.swift
@@ -11,13 +11,38 @@ import UIKit
 class ChatMessageCell: UITableViewCell {
 
     let messageLabel = UILabel()
+    let bubbleBackgroundView = UIView()
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         
+        bubbleBackgroundView.backgroundColor = .yellow
+        bubbleBackgroundView.layer.cornerRadius = 5
+        bubbleBackgroundView.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(bubbleBackgroundView)
+        
         addSubview(messageLabel)
-        messageLabel.backgroundColor = .green
-        messageLabel.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+//        messageLabel.backgroundColor = .green
+        messageLabel.text = "We want to provide a longer string that is actually going to wrap into the next line and maybe even a third line."
+        messageLabel.numberOfLines = 0
+        
+        messageLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        //lets setup some constraints for our label
+        let constraints = [messageLabel.topAnchor.constraint(equalTo: topAnchor, constant: 32),
+        messageLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 32),
+        messageLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -32),
+        messageLabel.widthAnchor.constraint(equalToConstant: 250),
+        
+        bubbleBackgroundView.topAnchor.constraint(equalTo: messageLabel.topAnchor, constant: -16),
+        bubbleBackgroundView.leadingAnchor.constraint(equalTo: messageLabel.leadingAnchor, constant: -16),
+        bubbleBackgroundView.bottomAnchor.constraint(equalTo: messageLabel.bottomAnchor, constant: 16),
+        bubbleBackgroundView.trailingAnchor.constraint(equalTo: messageLabel.trailingAnchor, constant: 16),]
+        
+        
+        NSLayoutConstraint.activate(constraints)
+        
+//        messageLabel.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
     }
     
     required init?(coder aDecoder: NSCoder) {
--- GroupedMessages/ViewController.swift
+++ GroupedMessages/ViewController.swift
@@ -11,6 +11,11 @@ import UIKit
 class ViewController: UITableViewController {
     
     fileprivate let cellId = "id123"
+    
+    let textMessages = [
+    "Here's my very first message",
+    "I'm going to message another long message that will word wrap",
+    "I'm going to message another long message that will word wrap.I'm going to message another long message that will word wrap.I'm going to message another long message that will word wrap"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,21 +23,22 @@ class ViewController: UITableViewController {
         navigationItem.title = "Messages"
         navigationController?.navigationBar.prefersLargeTitles = true
         
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
-        
-        
+        tableView.register(ChatMessageCell.self, forCellReuseIdentifier: cellId)
+        tableView.separatorStyle = .none
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return textMessages.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath) as! ChatMessageCell
         
-        cell.textLabel?.text = "We want to provide a longer string that is actually going to wrap into the next line and maybe even a third line."
+//        cell.textLabel?.text = "We want to provide a longer string that is actually going to wrap into the next line and maybe even a third line."
 
-        cell.textLabel?.numberOfLines = 0
+//        cell.textLabel?.numberOfLines = 0
+        
+        cell.messageLabel.text = textMessages[indexPath.row]
         return cell
     }
 
